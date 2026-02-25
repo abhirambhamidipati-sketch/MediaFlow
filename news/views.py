7ae3082372461c9cdbf552c49cb66dace0ee494e
Yash Pratap Singh <pratapyash234@gmail.com>
2026-02-25 13:58:19 +0530

    feat: added image upload and homepage

--- news/views.py
+++ news/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import ImageUpload
+
+def home(request):
+    images = ImageUpload.objects.all()
+    return render(request, 'home.html', {'images': images})
+
+from django.urls import path
+from .views import home
+
+urlpatterns = [
+    path('', home, name='home'),
+]
