35c278b8e9b7b438130d7b303d1e44e3fed106ea
Abhiram <abhiram.bhamidipati@gmail.com>
2026-03-29 17:04:19 +0530

    ci: added GitHub Actions pipeline and tests

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import News
 
-# Create your tests here.
+class NewsModelTest(TestCase):
+    def setUp(self):
+        self.news = News.objects.create(
+            title="Test News",
+            description="Test Description"
+        )
+
+    def test_news_creation(self):
+        self.assertEqual(self.news.title, "Test News")
+        self.assertEqual(self.news.description, "Test Description")
