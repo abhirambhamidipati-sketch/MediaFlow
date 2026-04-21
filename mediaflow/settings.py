8cfe30f1b7c8cc7733b0d7db8366731aabe9283b
Abhiram Bhamidipati <abhiram.bhamidipati@gmail.com>
2026-04-21 11:40:28 +0530

    Settings.py changed

--- mediaflow/settings.py
+++ mediaflow/settings.py
@@ -20,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 
